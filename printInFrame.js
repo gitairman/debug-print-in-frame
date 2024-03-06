ace88493c698c9ed42f727ea7ca56edc9994a01e
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-03-05 21:37:46 -0800

    added 4 more * to the borders and fixed bug in the longestStr function where it wasn't comparing the size of the strings in the for loop

--- printInFrame.js
+++ printInFrame.js
@@ -1,37 +1,36 @@
-const printInFrame = function(list) {
-  list = list.split(' ');
-  const longest = longestStr(list).length;
-  const border = repeat('*', longest);
+const printInFrame = function (list) {
+  list = list.split(' ')
+  const longest = longestStr(list).length
+  const border = '\n' + repeat('*', longest + 4)
 
-  console.log(border);
+  console.log(border)
   for (const word of list) {
-    console.log(`* ${word}${repeat(' ', longest - word.length + 1)}*`);
+    console.log(`\n* ${word}${repeat(' ', longest - word.length + 1)}*`)
   }
-  console.log(border);
-};
+  console.log(border)
+}
 
-const repeat = function(str, times) {
-  let result = str;
+const repeat = function (str, times) {
+  let result = str
 
   for (let i = 0; i < times; i++) {
-    result += str;
+    result += str
   }
 
-  return result;
-};
+  return result
+}
 
-const longestStr = function(list) {
-  let longest = list[0];
+const longestStr = function (list) {
+  let longest = list[0]
 
   for (const str of list) {
-    longest = str;
+    if (str.length > longest.length) longest = str
   }
-
-  return longest;
-};
+  return longest
+}
 
 // Test driver code, do not modify
-printInFrame('May the force be with you');
-printInFrame('Here\'s Johnny!');
-printInFrame('Supercalifragalisticexpialadocious');
-printInFrame('Lost, like tears in the rain');
+printInFrame('May the force be with you')
+printInFrame("Here's Johnny!")
+printInFrame('Supercalifragalisticexpialadocious')
+printInFrame('Lost, like tears in the rain')
